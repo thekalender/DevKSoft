4221693387c90bc4c1847547176b40ed047bb335
ahmetkalender93 <ahmetkalender93@outlook.com.tr>
2019-10-01 13:17:32 +0300

    ComplexTeype yapıldı

--- DevKSoft.Northwind.DataAccess/Abstract/IProductDal.cs
+++ DevKSoft.Northwind.DataAccess/Abstract/IProductDal.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DevKSoft.Core.DataAccess;
+using DevKSoft.Northwind.Entities.ComplexTypes;
 using DevKSoft.Northwind.Entities.Concrete;
 
 namespace DevKSoft.Northwind.DataAccess.Abstract
 {
     public interface IProductDal:IEntityRepository<Product>
     {
+        List<ProductDetail> GetProductDetails();
+
     }
 }
--- DevKSoft.Northwind.DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DevKSoft.Northwind.DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -5,11 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 using DevKSoft.Core.DataAccess.EntityFramework;
 using DevKSoft.Northwind.DataAccess.Abstract;
+using DevKSoft.Northwind.Entities.ComplexTypes;
 using DevKSoft.Northwind.Entities.Concrete;
 
 namespace DevKSoft.Northwind.DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : EfEntityRepositoryBase<Product,NorthwindContext>,IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
     {
+        public List<ProductDetail> GetProductDetails()
+        {
+            using (NorthwindContext context= new NorthwindContext())
+            {
+                var result = from p in context.Products
+                    join c in context.Categories on p.CategoryId equals c.CategoryId
+                    select new ProductDetail
+                    {
+                        ProductId = p.ProductId,
+                        ProductName = p.ProductName,
+                        CategoryName = c.CategoryName
+                    };
+                return result.ToList();
+            }
+        }
     }
 }
