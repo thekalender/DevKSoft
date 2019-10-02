c49555520f12bac9fa0bc7d29b53044dced64f16
ahmetkalender93 <ahmetkalender93@outlook.com.tr>
2019-10-02 12:19:29 +0300

    Mock ile business katmanı test edildi.

---
+++ DevKSoft.Northwind.Business.Tests/ProductManagerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DevKSoft.Northwind.Business.Concrete.Manager;
+using DevKSoft.Northwind.DataAccess.Abstract;
+using DevKSoft.Northwind.Entities.Concrete;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentValidation;
+using Moq;
+
+namespace DevKSoft.Northwind.Business.Tests
+{
+    [TestClass]
+    public class ProductManagerTests
+    {
+        [ExpectedException(typeof(ValidationException))]
+        [TestMethod]
+        public void Product_validation_check()
+        {
+            Mock<IProductDal> mock= new Mock<IProductDal>();
+            ProductManager productManager=new ProductManager(mock.Object);
+
+            productManager.Add(new Product());
+        }
+    }
+}
